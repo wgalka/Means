90a85830a712bb1a7554f7ae46392f0a80a14a7d
wgalka <wgalka2@op.pl>
2022-03-07 11:29:31 +0100

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='means',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Python implementation of Arithmetic, quasi arithmetic and other aggregating functions',
+    author='wgalka',
+    license='TO DOO',
+)
